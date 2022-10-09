a91a18b7992ecd618f22d96053e323ab459c6ee3
sushant102004 <sushant.dhiman9812@gmail.com>
2022-10-09 17:53:41 +0530

    Merge Two Sorted Array

---
+++ 001-Arrays/Questions/03-MergeTwoSortedArray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void mergerSortedArray(int arrOne[], int arrTwo[], int arrThree[], int m, int n){
+    int i = 0, j = 0, k = 0;
+    while(i < m && j < n){
+        if(arrOne[i] < arrTwo[j]){
+            arrThree[k++] = arrOne[i++];
+        } else {
+            arrThree[k++] = arrTwo[j++];
+        }
+    }
+
+    while(i < n){
+        arrThree[k++] = arrOne[i++];
+    }
+
+    while(j < m){
+        arrThree[k++] = arrTwo[j++];
+    }
+}
+
+int main(){
+    int arrOne[4] = {1, 3, 5, 7};
+    int arrTwo[3] = {2, 4, 6};
+    int arrThree[7] = {};
+
+    mergerSortedArray(arrOne, arrTwo, arrThree, 4, 3);
+
+    for(int i = 0; i < 6; i++){
+        cout << arrThree[i] << endl;
+    }
+    return 0;
+}
