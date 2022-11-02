0f70e5de639556dd27b4e8c06b189510c57a077a
sushant102004 <sushant.dhiman9812@gmail.com>
2022-11-02 06:26:23 +0530

    Move Zeros Updated

--- 001-Arrays/Questions/13-MoveZeros.cpp
+++ 001-Arrays/Questions/13-MoveZeros.cpp
@@ -9,38 +9,16 @@ Output: [1,3,12,0,0]
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> moveZeroes(vector<int>& nums) {
-    int zeros = 0;
-    vector<int> ans;
-
-    // Count Zeros
-    for(int i = 0; i < nums.size() - 1; i++){
-        if(nums[i] == 0) zeros++;
+void moveZeroes(vector<int>& nums) {
+       int i = 0;
+       for(int j = 0; j < nums.size(); j++){
+           if(nums[j] != 0){
+               swap(nums[j], nums[i]);
+               i++;
+           }
+       } 
     }
-
-    for(int i = 0; i < nums.size() - 1; i++){
-        if(nums[i] != 0){
-            ans.push_back(nums[i]);
-        }
-    }
-
-    while(zeros > 0){
-        ans.push_back(0);
-        zeros--;
-    }
-    return ans;
-}
 
 int main(){
-    vector<int> a;
-    a.push_back(0);
-    a.push_back(1);
-    a.push_back(0);
-    a.push_back(3);
-    a.push_back(12);
-
-    vector<int> ans =  moveZeroes(a);
-
-    for(auto i : a) cout << ans[i] << " ";
     return 0;
 }
